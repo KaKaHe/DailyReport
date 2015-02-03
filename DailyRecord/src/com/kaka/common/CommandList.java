2409093018cbb8f859cd321c745b7820d9ffb23c
KaKaHe <becker_heo@hotmail.com>
2015-02-03 17:29:15 -0500

    Submission of Feb 3rd, 2015. Daily Submission.

--- DailyRecord/src/com/kaka/common/CommandList.java
+++ DailyRecord/src/com/kaka/common/CommandList.java
@@ -19,7 +19,7 @@ public final class CommandList {
 	public static final String DR002_USERNAME = "UserName ";
 	public static final String DR002_PASSWORD = "Password ";
 	public static final String DR002_CONFIRMPASSWORD = "Confirm ";
-	public static final String DR002_PASSWORDINFO = "";
+	public static final String DR002_PASSWORDINFO = "8 digits Minimum";
 	public static final String DR002_SECURITY_QUESTION = "Security Question ";
 	public static final String DR002_SECURITY_ANSWER = "Security Answer ";
 	public static final String DR002_FIRSTNAME = "First Name ";
--- DailyRecord/src/com/kaka/dailyrecord/pages/RegisterWindow.java
+++ DailyRecord/src/com/kaka/dailyrecord/pages/RegisterWindow.java
@@ -54,42 +54,18 @@ public class RegisterWindow extends JFrame implements WindowListener, ActionList
 
 	private void initialFrame() {
 		// TODO Auto-generated method stub
-		JPanel jp_Credential = createCredentialPanel();
-		JPanel jp_Security = createSecurityPanel();
 		JPanel jp_Personal = createPersonalPanel();
+		JPanel jp_Credential = createCredentialPanel();
+		JPanel jp_SecurityControl = createSecurityControlPanel();
 		
 		//Set Global Font setting
 		//UI.setUIFont(new FontUIResource(new Font("TimesRoman", Font.BOLD, 18)));
 
-		this.add(jp_Personal, BorderLayout.CENTER);
-		this.add(jp_Credential, BorderLayout.CENTER);
-		this.add(jp_Security, BorderLayout.CENTER);
-	}
-	
-	/**
-	 * Create the Credential Information part in Register Window
-	 * @return	JPanel, which includes fields for UserName, Password, Confirm PWD, and an instruction label for password.
-	 */
-	private JPanel createCredentialPanel() {
-		JPanel jp_Credential = new JPanel(new GridLayout(4, 4, 0, 30));
-		//Border bRegister = BorderFactory.createLineBorder(Color.GRAY);
-		jp_Credential.setBorder(BorderFactory.createTitledBorder(bRegister, "Credential"));
-		jp_Credential.setFont(new Font("TimesRoman", Font.BOLD, 14));
+		this.setLayout(new GridLayout(3,1));
 		
-		return jp_Credential;
-	}
-	
-	/**
-	 * Create the Security Information part in Register Window
-	 * @return	JPanel, which includes fields for Security Question and Answer.
-	 */
-	private JPanel createSecurityPanel() {
-		JPanel jp_Security = new JPanel(new GridBagLayout());
-		//Border bRegister = BorderFactory.createLineBorder(Color.GRAY);
-		jp_Security.setBorder(BorderFactory.createTitledBorder(bRegister, "Security"));
-		jp_Security.setFont(new Font("TimesRoman", Font.BOLD, 14));
-		
-		return jp_Security;
+		this.add(jp_Personal);
+		this.add(jp_Credential);
+		this.add(jp_SecurityControl);
 	}
 	
 	/**
@@ -105,12 +81,10 @@ public class RegisterWindow extends JFrame implements WindowListener, ActionList
 																TitledBorder.DEFAULT_JUSTIFICATION, 
 																TitledBorder.DEFAULT_POSITION, 
 																fTitle)
-								);
+							  );
 		//jp_Personal.setFont(new Font("TimesRoman", Font.BOLD, 18));
 		
 		//First Name
-		//place holder
-		jp_Personal.add(new JLabel());
 		//label
 		JLabel jl_FirstName = new JLabel(CommandList.DR002_FIRSTNAME);
 		jp_Personal.add(jl_FirstName);
@@ -119,10 +93,10 @@ public class RegisterWindow extends JFrame implements WindowListener, ActionList
 		jp_Personal.add(jtf_FirstName);
 		//place holder
 		jp_Personal.add(new JLabel());
-		
-		//Last Name
 		//place holder
 		jp_Personal.add(new JLabel());
+		
+		//Last Name
 		//label
 		JLabel jl_LastName = new JLabel(CommandList.DR002_LASTNAME);
 		jp_Personal.add(jl_LastName);
@@ -131,25 +105,23 @@ public class RegisterWindow extends JFrame implements WindowListener, ActionList
 		jp_Personal.add(jtf_LastName);
 		//place holder
 		jp_Personal.add(new JLabel());
-		
-		//Gender
 		//place holder
 		jp_Personal.add(new JLabel());
+		
+		//Gender
 		//label
 		JLabel jl_Gender = new JLabel(CommandList.DR002_GENDER);
 		jp_Personal.add(jl_Gender);
-		//selectbox
-		//jp_Personal.add(new JLabel("For select gender."));	//placeholder, need to be removed later.
-		
+		//selectbox		
 		String[] strGender = new String[] { CommandList.DR002_GENDER_FEMALE, CommandList.DR002_GENDER_MALE, CommandList.DR002_GENDER_OTHER };
 		JComboBox<String> jcb_Gender = new JComboBox<String>(strGender);
 		jp_Personal.add(jcb_Gender);
 		//placeholder
 		jp_Personal.add(new JLabel());
+		//place holder
+		jp_Personal.add(new JLabel());
 		
 		//Age
-		//placeholder
-		jp_Personal.add(new JLabel());
 		//label
 		JLabel jl_Age = new JLabel(CommandList.DR002_AGE);
 		jp_Personal.add(jl_Age);
@@ -158,12 +130,132 @@ public class RegisterWindow extends JFrame implements WindowListener, ActionList
 		jp_Personal.add(jtf_Age);
 		//placeholder
 		jp_Personal.add(new JLabel());
+		//placeholder
+		jp_Personal.add(new JLabel());
 		
 		//Setup Font
 		//UI.setupFont(jp_Personal, new Font("TimesRoman", Font.BOLD, 16));
 		UI.setupFont(jp_Personal, fUsual);
 		
 		return jp_Personal;
+	}
+	
+	/**
+	 * Create the Credential Information part in Register Window
+	 * @return	JPanel, which includes fields for UserName, Password, Confirm PWD, and an instruction label for password.
+	 */
+	private JPanel createCredentialPanel() {
+		JPanel jp_Credential = new JPanel(new GridLayout(4, 4, 0, 10));
+		//Border bRegister = BorderFactory.createLineBorder(Color.GRAY);
+		//jp_Credential.setBorder(BorderFactory.createTitledBorder(bRegister, "Credential"));
+		jp_Credential.setBorder(BorderFactory.createTitledBorder(bRegister, 
+																  "Credential", 
+																  TitledBorder.DEFAULT_JUSTIFICATION, 
+																  TitledBorder.DEFAULT_POSITION, 
+																  fTitle)
+								);
+		//jp_Credential.setFont(new Font("TimesRoman", Font.BOLD, 14));
+		
+		//UserName
+		//label
+		JLabel jl_UserName = new JLabel(CommandList.DR002_USERNAME);
+		jp_Credential.add(jl_UserName);
+		//input field
+		JTextField jtf_UserName = new JTextField();
+		jp_Credential.add(jtf_UserName);
+		//placeholder
+		jp_Credential.add(new JLabel());
+		//placeholder
+		jp_Credential.add(new JLabel());
+		
+		//Password
+		//label
+		JLabel jl_Password = new JLabel(CommandList.DR002_PASSWORD);
+		jp_Credential.add(jl_Password);
+		//inputfield (masked)
+		JPasswordField jpf_Password = new JPasswordField();
+		jp_Credential.add(jpf_Password);
+		//placeholder
+		jp_Credential.add(new JLabel());
+		//placeholder
+		jp_Credential.add(new JLabel());
+		
+		//Confrim Password
+		//label
+		JLabel jl_Confirm = new JLabel(CommandList.DR002_CONFIRMPASSWORD);
+		jp_Credential.add(jl_Confirm);
+		//inputfield (masked)
+		JPasswordField jpf_Confirm = new JPasswordField();
+		jp_Credential.add(jpf_Confirm);
+		//placeholder
+		jp_Credential.add(new JLabel());
+		//placeholder
+		jp_Credential.add(new JLabel());
+		
+		//Password Instruction
+		//placeholder
+		jp_Credential.add(new JLabel());
+		//instruction
+		jp_Credential.add(new JLabel(CommandList.DR002_PASSWORDINFO));
+		//placeholder
+		jp_Credential.add(new JLabel());
+		//placeholder
+		jp_Credential.add(new JLabel());
+
+		UI.setupFont(jp_Credential, fUsual);
+		return jp_Credential;
+	}
+	
+	/**
+	 * Create the Security Information part in Register Window and the button panel
+	 * @return	JPanel, which includes fields for Security Question and Answer. It also includes the buttons, SUBMIT, RESET, CANCEL.
+	 */
+	private JPanel createSecurityControlPanel() {
+		JPanel jp_Security = new JPanel(new GridLayout(2, 2, 0, 10));
+		//Border bRegister = BorderFactory.createLineBorder(Color.GRAY);
+		//jp_Security.setBorder(BorderFactory.createTitledBorder(bRegister, "Security"));
+		jp_Security.setBorder(BorderFactory.createTitledBorder(bRegister, 
+																"Security", 
+																TitledBorder.DEFAULT_JUSTIFICATION, 
+																TitledBorder.DEFAULT_POSITION, 
+																fTitle)
+							  );
+		//jp_Security.setFont(new Font("TimesRoman", Font.BOLD, 14));
+		
+//		jp_Security.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+//		
+//		GridBagConstraints c = new GridBagConstraints();
+//		c.fill=GridBagConstraints.BOTH;
+//		c.insets = new Insets(10, 0, 10, 0);
+//		
+//		JLabel jl_Question = new JLabel(CommandList.DR002_SECURITY_QUESTION);
+//		c.gridx = 0;
+//		c.gridy = 0;
+//		jp_Security.add(jl_Question, c);
+//		
+//		JTextField jtf_Question = new JTextField();
+//		c.gridx = 1;
+//		c.gridy = 0;
+//		c.gridwidth = 3;
+//		jp_Security.add(jtf_Question, c);
+//		
+//		JLabel jl_Answer = new JLabel(CommandList.DR002_SECURITY_ANSWER);
+//		c.gridx = 0;
+//		c.gridy = 1;
+//		jp_Security.add(jl_Answer, c);
+//		
+//		JTextField jtf_Answer = new JTextField();
+//		c.gridx = 1;
+//		c.gridy = 1;
+//		c.gridwidth = 3;
+//		c.weightx = 1;
+//		jp_Security.add(jtf_Answer, c);
+		
+		//Security Question
+		//Security Answer
+		
+		UI.setupFont(jp_Security, fUsual);
+		return jp_Security;
 	}
 
 	/* (non-Javadoc)
